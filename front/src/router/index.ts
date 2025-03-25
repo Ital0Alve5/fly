f381d87bc64048c7df14855abaf27161382a9143
Juliano Filho <julianofilho@ufpr.br>
2025-03-25 20:58:32 -0300

    feat: página inicial do cliente com tabela de reservas

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,44 +1,52 @@
-import Auth from '@/views/Auth/index.vue'
-import Booking from '@/views/Client/Booking.vue'
-import Adm from '@/views/Manager/PaginaAdmTeste.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useGlobalStore } from '@/stores/global';
+import Auth from '@/views/Auth/index.vue';
+import Booking from '@/views/Client/Booking.vue';
+import Adm from '@/views/Manager/PaginaAdmTeste.vue';
+import ClientHome from '@/views/Client/ClientHome.vue';
 
-import { createRouter, createWebHistory } from 'vue-router'
-import { useGlobalStore } from '@/stores/global'
+const routes = [
+  {
+    path: '/',
+    name: 'auth',
+    component: Auth,
+  },
+  {
+    path: '/reservas',
+    name: 'reservas',
+    component: Booking,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/admTeste',
+    name: 'admTeste',
+    component: Adm,
+    meta: { requiresAuth: true, isManager: true },
+  },
+  {
+    path: '/cliente',
+    name: 'clienteHome',
+    component: ClientHome,
+    meta: { requiresAuth: false },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'auth',
-      component: Auth,
-    },
-    {
-      path: '/reservas',
-      name: 'reservas',
-      component: Booking,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/admTeste',
-      name: 'admTeste',
-      component: Adm,
-      meta: { requiresAuth: true, isManager: true },
-    },
-  ],
-})
+  routes,
+});
 
 router.beforeEach((to) => {
-  const globalStore = useGlobalStore()
-  const userData = globalStore.getAuthenticatedUserData()
+  const globalStore = useGlobalStore();
+  const userData = globalStore.getAuthenticatedUserData();
 
   if (
     (!userData.value && to.meta.requiresAuth) ||
     (userData.value && !userData.value.isManager && to.meta.isManager) ||
     (userData.value && userData.value.isManager && !to.meta.isManager && to.name !== 'auth')
   ) {
-    return { name: 'auth' }
+    return { name: 'auth' };
   }
-})
+});
 
-export default router
+export default router;
---
+++ front/src/views/Client/ClientHome.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ClientDashboard from '@/components/features/Auth/ClientDashboard.vue';
+</script>
+
+<template>
+  <ClientDashboard />
+</template>
