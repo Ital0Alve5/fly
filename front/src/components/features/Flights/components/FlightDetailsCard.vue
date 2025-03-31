656de8d7f3eaca898a7612e411a257e408b6b5ac
YasmimZedelinski-Atlas <yasmim.zedelinski@atlastechnol.com>
2025-03-31 20:54:59 -0300

    feat: adiciona os dois componentes da página

---
+++ front/src/components/features/Flights/components/FlightDetailsCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+</script>
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>Detalhes do voo</CardTitle>
+      <CardDescription>Veja mais detalhes do voo abaixo:</CardDescription>
+    </CardHeader>
+    <CardContent>
+      
+    </CardContent>
+  </Card>
+</template>
--- front/src/views/Flights/FlightDetails.vue
+++ front/src/views/Flights/FlightDetails.vue
@@ -1,2 +1,17 @@
-<script lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import FlightDetailsCard from '@/components/features/Flights/components/FlightDetailsCard.vue'
+import FlightingPayment from '@/components/features/Flights/components/FlightingPayment.vue'
+</script>
+<template>
+  <main class="flex items-center mt-36 flex-col">
+    <section>
+      <div class="flex flex-col">
+        <h1 class="text-4xl font-bold">Detalhes do voo</h1>
+      </div>
+    </section>
+    <section>
+      <FlightDetailsCard />
+      <FlightingPayment />
+    </section>
+  </main>
+</template>
