ac91544969beb3b70242cf0fb5e9ef8c6eac9471
Heitor Plinta de Oliveira <heitor.hes@hotmail.com>
2025-04-01 15:12:53 -0300

    fix: troca stores de options pra setup

--- front/src/stores/user.ts
+++ front/src/stores/user.ts
@@ -1,12 +1,16 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useUserInfoStore = defineStore('userInfo', {
-  state: () => ({
-    userId: null as number | null
-  }),
-  actions: {
-    setUserId(id: number) {
-      this.userId = id
-    }
+export const useUserInfoStore = defineStore('userInfo', () => {
+
+  const userId = ref<number | null>(null)
+
+  function setUserId(id: number) {
+    userId.value = id
+  }
+
+  return {
+    userId,
+    setUserId
   }
 })
