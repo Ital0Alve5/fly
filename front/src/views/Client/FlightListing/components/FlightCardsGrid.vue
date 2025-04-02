420e63800887621d8495ece4254808d227b4cfaa
Tomaz Xavier <tomazcx06@gmail.com>
2025-04-01 23:43:01 -0300

    fix: flights mock search, added flights and improved empty list feedback

--- front/src/views/Client/FlightListing/components/FlightCardsGrid.vue
+++ front/src/views/Client/FlightListing/components/FlightCardsGrid.vue
@@ -5,13 +5,17 @@ import type { Flight } from '@/mock/flight'
 defineProps<{
   flights: Flight[]
   loading: boolean
+  fetchedFlights: boolean
 }>()
 </script>
 
 <template>
-  <div class="grid grid-cols-12 mt-4 gap-4 w-11/12 max-w-5xl">
+  <div class="grid grid-cols-12 my-4 gap-4 w-11/12 max-w-5xl">
     <template v-if="loading">
       <Skeleton class="col-span-3 h-[229px] w-full rounded-xl" v-for="number in 8" :key="number" />
+    </template>
+    <template v-else-if="fetchedFlights && flights.length === 0">
+      <span class="text-center text-3xl mt-8 col-span-12">Nenhum vôo encontrado!</span>
     </template>
     <template v-else>
       <slot v-for="flight in flights" :flight="flight" />
