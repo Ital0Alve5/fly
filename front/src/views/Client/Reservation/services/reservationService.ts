4c6b84e092eb8bfebe0c4f7cc748dc93d74e7619
YasmimZedelinski <yasmim.zedelinski@gmail.com>
2025-05-14 21:07:55 -0300

    feat: adiciona service de reservas

---
+++ front/src/views/Client/Reservation/services/reservationService.ts
@@ -0,0 +1,37 @@
+import api from '@/lib/axios'
+import type { Reserve } from '@/types/Reserve'
+import type { AxiosResponse } from 'axios'
+
+export async function listReservationsByClient(): Promise<AxiosResponse<Reserve[]>> {
+  const stored = localStorage.getItem('auth/user')!
+  const user = JSON.parse(stored)
+  const clientCode = user.usuario.codigo
+  return await api.get<Reserve[]>(`/clientes/${clientCode}/reservas`)
+}
+
+export async function getReservationByCode(code: string): Promise<AxiosResponse<Reserve>> {
+  return await api.get<Reserve>(`/reservas/${code}`)
+}
+
+export async function createReservation(data: {
+  codigo_cliente: number
+  valor: number
+  milhas_utilizadas: number
+  quantidade_poltronas: number
+  codigo_voo: string
+  codigo_aeroporto_origem: string
+  codigo_aeroporto_destino: string
+}): Promise<AxiosResponse<Reserve>> {
+  return await api.post<Reserve>('/reservas', data)
+}
+
+export async function cancelReservationService(code: string): Promise<AxiosResponse<void>> {
+  return await api.delete<void>(`/reservas/${code}`)
+}
+
+export async function updateReservationStateService(
+  code: string,
+  estado: 'CHECK-IN' | 'EMBARCADA' | 'CANCELADA'
+): Promise<AxiosResponse<void>> {
+  return await api.patch<void>(`/reservas/${code}/estado`, { estado })
+}
